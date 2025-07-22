e3770d8424312118e7a020901074e45c3b18ce6e
pavamanaviation <pavaman.official@gmail.com>
2025-07-22 15:08:59 +0530

    back

--- invest_proj/invest_proj/celery.py
+++
@@ -1,9 +0,0 @@
-# invest_proj/celery.py
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invest_proj.settings')
-
-app = Celery('invest_proj')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
